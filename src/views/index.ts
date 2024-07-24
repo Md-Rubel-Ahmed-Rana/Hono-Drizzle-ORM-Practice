ce982cf89a5ac6bebc5b278cd17cd40158bd599c
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-07-24 22:58:47 +0600

    HTML code splitted

--- src/views/index.ts
+++ src/views/index.ts
@@ -3,43 +3,70 @@ import path from "path";
 import fs from "fs";
 import { authenticate } from "../middlewares/authenticate";
 import { unauthenticate } from "../middlewares/unauthenticate";
+import { generateHtmlMetadata } from "../utils/generateHtmlMetadata";
 
 const router = new Hono();
+const headFilePath = path.join(__dirname, "./shared", "head.html");
+const head = fs.readFileSync(headFilePath, "utf-8");
 
 router.get("/", authenticate, (c) => {
   const filePath = path.join(__dirname, "./", "index.html");
-  const htmlFile = fs.readFileSync(filePath, "utf-8");
-  return c.html(htmlFile);
+  const body = fs.readFileSync(filePath, "utf-8");
+  const html = generateHtmlMetadata("News Feeds", body);
+  return c.render(`
+    ${head}
+    ${html}
+    `);
 });
 
 router.get("/register", unauthenticate, (c) => {
-  const filePath = path.join(__dirname, "./", "register.html");
-  const htmlFile = fs.readFileSync(filePath, "utf-8");
-  return c.html(htmlFile);
+  const registerPageFilePath = path.join(__dirname, "./", "register.html");
+  const body = fs.readFileSync(registerPageFilePath, "utf-8");
+  const html = generateHtmlMetadata("Register", body);
+  return c.render(`
+    ${head}
+    ${html}
+    `);
 });
 
 router.get("/login", unauthenticate, (c) => {
-  const filePath = path.join(__dirname, "./", "login.html");
-  const htmlFile = fs.readFileSync(filePath, "utf-8");
-  return c.html(htmlFile);
+  const loginPageFilePath = path.join(__dirname, "./", "login.html");
+  const body = fs.readFileSync(loginPageFilePath, "utf-8");
+  const html = generateHtmlMetadata("Login", body);
+  return c.render(`
+    ${head}
+    ${html}
+    `);
 });
 
 router.get("/new-post", authenticate, (c) => {
   const filePath = path.join(__dirname, "./", "new-post.html");
-  const htmlFile = fs.readFileSync(filePath, "utf-8");
-  return c.html(htmlFile);
+  const body = fs.readFileSync(filePath, "utf-8");
+  const html = generateHtmlMetadata("New Post", body);
+  return c.render(`
+    ${head}
+    ${html}
+    `);
 });
 
 router.get("/posts/post/edit/:id", authenticate, (c) => {
   const filePath = path.join(__dirname, "./", "edit-post.html");
-  const htmlFile = fs.readFileSync(filePath, "utf-8");
-  return c.html(htmlFile);
+  const body = fs.readFileSync(filePath, "utf-8");
+  const html = generateHtmlMetadata("Edit post", body);
+  return c.render(`
+    ${head}
+    ${html}
+    `);
 });
 
 router.get("/profile", authenticate, (c) => {
   const filePath = path.join(__dirname, "./", "profile.html");
-  const htmlFile = fs.readFileSync(filePath, "utf-8");
-  return c.html(htmlFile);
+  const body = fs.readFileSync(filePath, "utf-8");
+  const html = generateHtmlMetadata("Profile ", body);
+  return c.render(`
+    ${head}
+    ${html}
+    `);
 });
 
 export const generateNotFoundHtml = (c: Context) => {
